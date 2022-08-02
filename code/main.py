88aee15de47b17295159f4db889ab966b009fc85
jose clavo tafur <jclavotafur@gmail.com>
2022-08-02 09:09:00 -0300

    basic example

--- code/main.py
+++ code/main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+
+import pandas as pd
+import numpy as np
+
+technologies= {
+    'Courses':["Spark","PySpark","Hadoop","Python","Pandas"],
+    'Fee' :[22000,25000,23000,24000,26000],
+    'Duration':['30days','50days','30days', None,np.nan],
+    'Discount':[1000,2300,1000,1200,2500]
+          }
+df = pd.DataFrame(technologies)
+print(df)
